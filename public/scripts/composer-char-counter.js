96918a3eed657f712daae7f603a4fe96144c1980
Hanson Lee <leehanson@alumni.ubc.ca>
2022-10-12 04:47:58 +0000

    Added hover animations for tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   $('#tweet-text').on("input", function() {
-    let inputLength = $(this).val().length;
-    if (140 - inputLength >= 0) {
+    let inputLength = $(this).val().length; //Gets character amount in form
+    
+    if (140 - inputLength >= 0) { //Function for if above character limit, text renders black (styled in css)
       $('#charcounter').attr('class', 'counter');
       $(this).next().find('.counter').html(140 - inputLength);
     }
     
 
-    if (140 - inputLength < 0) {
+    if (140 - inputLength < 0) { //Function for if over character limit, text renders red (styled in css)
       $('#charcounter').attr('class', 'redcounter');
       $(this).next().find('.redcounter').html(140 - inputLength);
     }
